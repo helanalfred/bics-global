7b281822ba14c4a93f4daeddac2d78cab40b7d29
helanalfred <helanalfred2002@gmail.com>
2024-07-15 22:21:09 +0530

    table change by js

---
+++ tablejs.js
@@ -0,0 +1,13 @@
+document.write("<table> <thead> <th>first name</th><th>last name</th><th>gender</th></thead>");
+for(var i=0;i<1;i++)
+{
+    for(var j=0;j<1;j++)
+    {
+        document.write("<tr><td>sumeera</td><td>jeelankhan</td><td>female</td></tr>");
+        document.write("<tr><td>hindu</td><td>mathi</td><td>female</td></tr>");
+        document.write("<tr><td>sowmiya</td><td>sankaran</td><td>female</td></tr>");
+    };    
+}  
+document.write("</table>");
+
+
